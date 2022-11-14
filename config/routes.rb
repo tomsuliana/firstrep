66168407c63c260113bfea81ab120d16d8fe9e9c
tomsuliana <ultomskaya@yandex.ru>
2022-11-14 23:43:53 +0300

    Updated root path

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'example/input'
+  root "example#input"
   get 'example/show'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
