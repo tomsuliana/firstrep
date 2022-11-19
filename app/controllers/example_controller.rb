f40605cfef8f3ee1742213c3479b9dcd7d3b27d0
tomsuliana <ultomskaya@yandex.ru>
2022-11-19 21:26:31 +0300

    Tests done

--- app/controllers/example_controller.rb
+++ app/controllers/example_controller.rb
@@ -1,9 +1,50 @@
 class ExampleController < ApplicationController
+
   def input
   end
 
   def show
-    @digit = (1..params[:repeater].to_i).map {params[:digit]}.join
-    @num = params[:digit].to_i
+    num = params[:digit]
+    flash[:error] = check(num)
+    
+    if !flash[:error].nil?
+      redirect_to root_path
+    end
+
+    @arr = palindroms(num.to_i)
+
+  end
+
+  private
+
+  def is_palindrom(a) 
+    temp = a
+    b = 0
+    while (temp!=0) 
+      b = b *10 + temp % 10
+      temp /=10
+    end
+    a == b
+  end
+
+
+
+  def palindroms(num)
+    mas = []
+    num.times do |i|
+      if is_palindrom(i) && is_palindrom(i*i) then
+        mas.push(i)
+      end
+    end
+    mas
+  end
+
+  def check(num)
+    if num.nil? || num.empty?
+      return 'Empty string'
+    end
+    if !num.match(/^\d+$/)
+      return 'Enter numbers only'
+    end
   end
 end
--- spec/requests/example_spec.rb
+++ spec/requests/example_spec.rb
@@ -1,18 +1,56 @@
 require 'rails_helper'
+require 'nokogiri'
 
 RSpec.describe "Examples", type: :request do
   describe "GET /input" do
     it "returns http success" do
-      get "/example/input"
+      get root_path
       expect(response).to have_http_status(:success)
     end
   end
 
   describe "GET /show" do
     it "returns http success" do
-      get "/example/show"
+      get "/example/show", params: {digit:78}
       expect(response).to have_http_status(:success)
     end
   end
+    context 'shouldn`t redirect and return http Error if' do
+      it 'redirecting was without params' do
+        get '/example/show'
+        expect(response).to have_http_status(302)
+      end
 
+      it 'input 88uu ' do
+        get '/example/show', params: {digit: '67ff'}
+        expect(response).to have_http_status(302)
+      end
+
+      it 'input h7' do
+        get '/example/show', params: {digit: 'h7'}
+        expect(response).to have_http_status(302)
+      end
+      it 'input words' do
+        get '/example/show', params: {digit: 'ubuntu'}
+        expect(response).to have_http_status(302)
+      end
+    end
+
+    context 'should return body contains' do
+      it 'number 78' do
+        get '/example/show', params: {digit: 78}
+        html = Nokogiri::HTML(response.body)
+        expects = [0,0,1,1,2,4,3,9,11,121,22,484]
+        expects.each_with_index do |el, idx|
+          div = html.search('td')[idx.to_i].text.to_i
+          expect(div).to eq(el)
+        end
+      end
+      it 'number 78' do
+        get '/example/show', params: {digit: 78}
+        html = Nokogiri::HTML(response.body)
+        div = html.search('h3').text
+        expect(div).to eq("Количество: 6 ")
+      end
+    end
 end
