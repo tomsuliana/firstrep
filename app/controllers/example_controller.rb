71c2267ddf927a7f3b5561eb6da61ead5e2a6c78
tomsuliana <ultomskaya@yandex.ru>
2022-11-29 09:57:36 +0300

    Final commit

--- app/controllers/example_controller.rb
+++ app/controllers/example_controller.rb
@@ -1,50 +1,36 @@
-class ExampleController < ApplicationController
+# frozen_string_literal: true
 
-  def input
-  end
+# Class for controller
+class ExampleController < ApplicationController
+  def input; end
 
   def show
     num = params[:digit]
     flash[:error] = check(num)
-    
-    if !flash[:error].nil?
-      redirect_to root_path
-    end
+
+    redirect_to root_path unless flash[:error].nil?
 
     @arr = palindroms(num.to_i)
-
   end
 
   private
 
-  def is_palindrom(a) 
-    temp = a
-    b = 0
-    while (temp!=0) 
-      b = b *10 + temp % 10
-      temp /=10
-    end
-    a == b
+  def palindrom(aaa)
+    stra = aaa.to_s
+    stra.reverse == stra
   end
-
-
 
   def palindroms(num)
     mas = []
-    num.times do |i|
-      if is_palindrom(i) && is_palindrom(i*i) then
-        mas.push(i)
-      end
+    num.times do |iter|
+      mas.push(iter) if palindrom(iter) && palindrom(iter * iter)
     end
     mas
   end
 
   def check(num)
-    if num.nil? || num.empty?
-      return 'Empty string'
-    end
-    if !num.match(/^\d+$/)
-      return 'Enter numbers only'
-    end
+    return 'Empty string' if num.nil? || num.empty?
+
+    'Enter numbers only' unless num.match(/^\d+$/)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root "example#input"
-  get 'example/show'
+  get 'example/show', as: 'result'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
