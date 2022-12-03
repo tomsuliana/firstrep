e3cdd9764bdf299c0ea6ef5d3db7d45b63bd3e34
tomsuliana <ultomskaya@yandex.ru>
2022-12-03 21:36:41 +0300

    Testing finished

--- spec/system/palindrom_spec.rb
+++ spec/system/palindrom_spec.rb
@@ -1,16 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe 'Static content', type: :system do
-  let (:digit) { Faker::Number.number(digits: 2)}
+  let (:digit) {78}
   scenario 'normal working' do
     visit root_path
     fill_in :digit, with: digit
     find('#palindrom-btn').click
-    expect(find('#number-container')).to have_text("Количество: #{@arr.length}")
+    expect(find('#number-container')).to have_text("Количество: 6")  
   end
-
-  # scenario 'do not fill any values in form and click submit' do
-  #   visit root_path
-  #   find('#palindrom-btn').click
-  #   end
+  scenario 'fill nothing in form and click submit' do
+    visit root_path
+    fill_in :digit, with: ''
+    find('#palindrom-btn').click
+    expect(find('#error-result')).to have_text('Empty string')
+  end
+  scenario 'fill wrong values in form and click submit' do
+    visit root_path
+    fill_in :digit, with: 'uuu'
+    find('#palindrom-btn').click
+    expect(find('#error-result')).to have_text('Enter numbers only')
+  end
 end
