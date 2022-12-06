ab820cc98644bdce7ce3dd1675a4d914cac2fe3d
tomsuliana <ultomskaya@yandex.ru>
2022-12-06 13:32:12 +0300

    Scaffold and controller added

---
+++ app/controllers/session_controller.rb
@@ -0,0 +1,10 @@
+class SessionController < ApplicationController
+  def login
+  end
+
+  def authorize
+  end
+
+  def logout
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'session/login'
+  get 'session/authorize'
+  get 'session/logout'
+  resources :users
   root "example#input"
   post 'example/show', as: 'result'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
